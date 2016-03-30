fe556876a2387e449a3e6a1d8526cbbf3f191dde
khendrick21 <k.hendrick@tcu.edu>
2016-03-29 20:55:53 -0500

    get the username

---
+++ get_name.s
@@ -0,0 +1,37 @@
+##############################################
+#
+# READ ONLY SECTION
+#
+##############################################
+        .section        .rodata
+fmt_string: .string "%s"
+
+###############################################
+#
+# Text (Code) Segment
+#
+###############################################
+        .text
+        .globl 	get_name
+        .type   get_name, @function
+
+
+###############################################
+
+
+get_name:
+        pushq   %rbp
+        movq    %rsp, %rbp
+
+        mov     $user_name, %rax
+        mov     %rax, %rsi
+        mov     $fmt_string, %rdi
+        movq    $0, %rax
+        call    __isoc99_scanf
+
+        call    getchar
+
+        popq    %rbp
+        ret
+
+
